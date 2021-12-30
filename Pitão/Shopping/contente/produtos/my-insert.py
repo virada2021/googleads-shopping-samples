9f99fd60407c9815c9a52cc1ca943c8938e64768
virada2021 <96831331+virada2021@users.noreply.github.com>
2021-12-30 00:31:09 -0300

    Create my-insert.py

---
+++ Pitão/Shopping/contente/produtos/my-insert.py
@@ -0,0 +1,6 @@
+from __future__ import print_function
+import sys
+
+# The common module provides setup functionality used by the samples,
+# such as authentication and unique id generation.
+from shopping.content import common
